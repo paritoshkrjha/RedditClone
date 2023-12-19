2e113bddacc4fefcd5fe331f0d31cd6d2ee6bdd4
Paritosh Jha <paritoshjhaparitoshjha0504@gmail.com>
2023-12-19 15:37:47 +0530

    Added user state persistance

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/repository/auth_repository.dart';
@@ -14,6 +15,16 @@ final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   ),
 );
 
+final authStateChangeProvider = StreamProvider((ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.authStateChange;
+});
+
+final getUserDataProvider = StreamProvider.family((ref, String uid) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.getUserData(uid);
+});
+
 class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
   final Ref _ref;
@@ -24,6 +35,8 @@ class AuthController extends StateNotifier<bool> {
         _ref = ref,
         super(false); //loading
 
+  Stream<User?> get authStateChange => _authRepository.authStateChange;
+
   void signInWithGoogle(BuildContext context) async {
     state = true;
     final user = await _authRepository.signInWithGoogle();
@@ -32,5 +45,9 @@ class AuthController extends StateNotifier<bool> {
         (l) => showSnackBar(context, l.message),
         (userModel) => _ref.read(userProvider.notifier).update((state) =>
             userModel)); // l => left for failure, r => right for success
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _authRepository.getUserData(uid);
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,15 @@
+//loggedOut routes
+//loggedIn routed
+
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart.dart';
+import 'package:reddit_clone/features/home/screens/home_screen.dart';
+import 'package:routemaster/routemaster.dart';
+
+final loggedOutRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: LoginScreen()),
+});
+
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+});
